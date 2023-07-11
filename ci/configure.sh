e032a735c8c0a7aa2faa2c77be7030ef716602b8
Joseph Palermo <jpalermo@pivotal.io>
2023-07-11 15:46:16 +0800

    Move pipeline to bosh concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $(lpass status -q; echo $?) != 0 ]]; then
-  echo "Login with lpass first"
-  exit 1
-fi
-
-fly -t bosh-ecosystem set-pipeline -p "bosh-gcs-cli" \
+fly -t bosh set-pipeline -p "bosh-gcs-cli" \
     -c "$(dirname "${0}")/pipeline.yml"
